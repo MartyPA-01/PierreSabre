af8f675e13021193ca4a6fab50be1821f2ea4676
MartyPA-01 <martial.de-baecker@univ-tlse3.fr>
2025-03-27 11:02:01 +0100

    Création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,52 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFav;
+	private int argent;
+	
+	public Humain(String n,String boisson,int arg) {
+		this.nom=n;
+		this.boissonFav=boisson;
+		this.argent=arg;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle "+ getNom() + " et j’aime boire du "+ this.boissonFav);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de "+ this.boissonFav + " ! GLOUPS !");
+	}
+
+	private void gagnerArgent(int gain) {
+		this.argent+=gain;
+	}
+	
+
+	private void perdreArgent(int perte) {
+		this.argent-=perte;
+	}
+	
+	private void parler(String texte) {
+		System.out.println(String.format("(%s) - %s", getNom(),texte));
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix <=getArgent()) {
+			parler(String.format("J'ai %d sous en poche. Je vais pouvoir m'offrir %s à %d sous", this.argent,bien,prix));
+			perdreArgent(prix);
+		}
+		else {
+			parler(String.format("Je n'ai plus que %d sous en poche. Je ne peux même pas m'offrir %s à %d sous", this.argent,bien,prix));
+		}
+	}
+}
